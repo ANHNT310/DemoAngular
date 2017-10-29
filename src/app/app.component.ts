c325fb6604e7d96895c345655371c90103e86f35
SERVER-ANT\Anhnt <anhnt310@outlook.com>
2017-10-29 16:13:55 +0700

    view table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {DataSource} from '@angular/cdk/collections';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'learn angular anhnt';
+  displayedColumns = ['position', 'name', 'mail'];
+  dataSource = new ExampleDataSource();
+}
+
+export interface Element {
+  name: string;
+  position: string;
+  mail: string;
+}
+
+const data: Element[] = [
+  {position: 'dev', name: 'Hydrogen', mail: 'abc@abc'},
+  {position: 'dev', name: 'Hydrogen', mail: 'abc@abc'},
+  {position: 'dev', name: 'Hydrogen', mail: 'abc@abc'},
+
+];
+
+export class ExampleDataSource extends DataSource<any> {
+  /** Connect function called by the table to retrieve one stream containing the data to render. */
+  connect(): Observable<Element[]> {
+    return Observable.of(data);
+  }
+
+  disconnect() {}
 }
